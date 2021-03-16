7bf3b8f5ebf8262fe762a812e9cbcefb7e138e96
rosmarrc <79125757+rosmarrc@users.noreply.github.com>
2021-03-16 12:30:11 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace pract_4_eje_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nombres;
+            int x;
+            do
+            {
+                Console.WriteLine("cuantos nombres va a introducir?");
+                x = int.Parse(Console.ReadLine());
+            } while (x>100 );
+            nombres = new string[x];
+
+
+            for (int y = 0; y < x; y++)
+            {
+                Console.WriteLine("introduzca un nombre:");
+                nombres[y] = Console.ReadLine();
+            }
+           
+            Console.Clear();
+            Console.WriteLine("Listado de nombres introducidos");
+
+            foreach(string elemento in nombres)
+            {
+                Console.WriteLine(elemento);               
+            }
+            Console.ReadKey();
+        }
+    }
+}

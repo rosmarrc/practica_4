da78d7dec22fc2d0b338aae1451ca424e51e2b37
rosmarrc <79125757+rosmarrc@users.noreply.github.com>
2021-03-16 12:32:01 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace pract_4_eje_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.MinValue;
+            int y;
+            int z=0;
+            do
+            {
+                Console.WriteLine("inserte un numero");
+                y = int.Parse(Console.ReadLine());
+                if (y > x)
+                {
+                    x = y;
+                }
+                z++;
+            } while (z < 10);
+
+            Console.WriteLine("El mayor es: "+x);
+            Console.ReadKey();
+        }
+    }
+}

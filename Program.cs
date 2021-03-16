74356af0282ba202f490bf0f4c4de0dae6b87c07
rosmarrc <79125757+rosmarrc@users.noreply.github.com>
2021-03-16 12:36:13 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace pract_4_eje_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DELETREADOR DE NOMBRES"+Environment.NewLine);
+            Console.Write("Inserte su nombre: ");
+            string nombre = Console.ReadLine();
+            char[] letras = nombre.ToCharArray();
+            Console.Write("Nombre deletreado: ");
+            foreach(char elemento in letras)
+            { Console.Write($"{elemento} "); }
+
+            Console.ReadKey();
+        }
+    }
+}

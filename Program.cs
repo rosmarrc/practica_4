132868a30f4ff8b56c69ff9fe68f47345ffc40fb
rosmarrc <79125757+rosmarrc@users.noreply.github.com>
2021-03-16 12:43:45 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace pract_4_eje_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor;
+            string nombre;
+            Console.WriteLine("VERIFICADOR DE EDAD");
+            do
+            {
+                Console.Clear();
+                Console.Write("Inserte el nombre(pulsa enter para salir): ");
+                nombre = Console.ReadLine();
+                if(nombre=="")
+                { return; }
+                Console.Write("inserte edad:");
+                valor = int.Parse(Console.ReadLine());
+                if (valor < 0)
+                { Console.WriteLine("edad no valida"); }
+                else if (valor >= 18)
+                {
+                    Console.WriteLine($"Datos introducidos: \nNombre: {nombre} \nEdad: {valor}" + Environment.NewLine);
+                    Console.WriteLine($"Resultado: {nombre} es mayor de edad");
+                }
+                else
+                {
+                    Console.WriteLine($"Datos introducidos: \nNombre: {nombre} \nEdad: {valor}" + Environment.NewLine);
+                    Console.WriteLine($"Resultado: {nombre} es menor de edad");
+                }
+                Console.ReadKey();
+
+            } while (valor != -1);
+            Console.WriteLine("saliendo ...");
+            Console.ReadKey();
+        }
+    }
+}
